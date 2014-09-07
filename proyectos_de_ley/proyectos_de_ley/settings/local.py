95a3370559937b24b9c9110ec93fe3876403a5c2
aniversarioperu <aniversarioperu1@gmail.com>
2014-09-07 11:48:32 +0300

    start migration to django 1.7

---
+++ proyectos_de_ley/proyectos_de_ley/settings/local.py
@@ -0,0 +1,47 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import json
+
+from django.core.exceptions import ImproperlyConfigured
+from .base import *
+
+from unipath import Path
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+
+BASE_DIR = Path(__file__).absolute().ancestor(3)
+SECRETS_FILE = os.path.join(BASE_DIR.ancestor(1), 'config.json')
+
+with open(SECRETS_FILE) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret("SECRET_KEY")
+
+
+# Database needed for ``pdf`` app
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'leyes_sqlite3.db'),
+    }
+}
+
+# optional for scrapper
+CRAWLERA_USER = get_secret("CRAWLERA_USER")
+CRAWLERA_PASS = get_secret("CRAWLERA_PASS")
