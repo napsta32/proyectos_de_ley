eab5000cbb21502a061595107b6716fff2ce52d7
aniversarioperu <aniversarioperu1@gmail.com>
2014-09-08 23:31:22 +0300

    start templates

--- proyectos_de_ley/proyectos_de_ley/urls.py
+++ proyectos_de_ley/proyectos_de_ley/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+# from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'proyectos_de_ley.views.home', name='home'),
+    url(r'^$', include('pdl.urls', namespace='pdl')),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
