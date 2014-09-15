bf6f5b879860c4accdc1b189a2a29db5ba1cb92a
aniversarioperu <aniversarioperu1@gmail.com>
2014-09-15 12:56:09 +0300

    test all sitemaps

--- proyectos_de_ley/pdl/tests/test_sitemaps.py
+++ proyectos_de_ley/pdl/tests/test_sitemaps.py
@@ -8,10 +8,11 @@ from django.test import Client
 from django.test import TestCase
 
 from pdl.models import Proyecto
+from pdl.models import Slug
 
 
 class SimpleTest(TestCase):
-    def test_feed(self):
+    def test_proyect_items(self):
         this_folder = os.path.abspath(os.path.dirname(__file__))
         dummy_db_json = os.path.join(this_folder, 'dummy_db.json')
         dummy_items = json.loads(open(dummy_db_json, 'r').read())
@@ -22,3 +23,10 @@ class SimpleTest(TestCase):
         response = c.get('/sitemap.xml')
         soup = BeautifulSoup(response.content)
         self.assertEqual(1, len(soup.find_all('url')))
+
+        nombre = 'Zerillo Bazalar, Manuel Salvador'
+        slug = 'zerillo_bazalar_manuel/'
+        b = Slug(nombre=nombre, slug=slug)
+        b.save()
+        response = c.get('/sitemap.xml')
+        self.assertTrue(slug.encode('utf-8') in response.content)
