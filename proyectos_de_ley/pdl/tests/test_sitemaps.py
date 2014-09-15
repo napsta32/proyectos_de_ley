17d88c7536c2524fd1f217e5f52693f174bf902f
aniversarioperu <aniversarioperu1@gmail.com>
2014-09-15 11:46:56 +0300

    test sitemap

---
+++ proyectos_de_ley/pdl/tests/test_sitemaps.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+import json
+import os
+
+from bs4 import BeautifulSoup
+
+from django.test import Client
+from django.test import TestCase
+
+from pdl.models import Proyecto
+
+
+class SimpleTest(TestCase):
+    def test_feed(self):
+        this_folder = os.path.abspath(os.path.dirname(__file__))
+        dummy_db_json = os.path.join(this_folder, 'dummy_db.json')
+        dummy_items = json.loads(open(dummy_db_json, 'r').read())
+        b = Proyecto(**dummy_items[0])
+        b.save()
+
+        c = Client()
+        response = c.get('/sitemap.xml')
+        soup = BeautifulSoup(response.content)
+        self.assertEqual(1, len(soup.find_all('url')))
