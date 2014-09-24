01a43aaf9196e679cb9ddb777c4b8b46dca77c0e
aniversarioperu <aniversarioperu1@gmail.com>
2014-09-24 21:11:18 +0300

    no need for basecommand class

--- proyectos_de_ley/pdl/management/commands/update_seguimientos.py
+++ proyectos_de_ley/pdl/management/commands/update_seguimientos.py
@@ -2,13 +2,25 @@
 database and looks of new events in the `seguimiento_page`. It tries to
 update our database with new `seguimiento` events for each `proyecto`."""
 
-from django.core.management.base import NoArgsCommand, CommandError
+from django.core.management.base import BaseCommand, CommandError
+from pdl.management.commands.scraper import Command as ScraperCommand
 
 from pdl.models import Proyecto
 
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
+class Command(ScraperCommand):
+    """Inherits some methods from our scraper class."""
+    def handle(self, *args, **options):
+        self.tor = False
+
         proyectos = Proyecto.objects.all()
         for i in proyectos:
-            print(i.codigo, i.seguimiento_page)
+            codigo = i.codigo
+            soup = self.get(i.seguimiento_page)
+            events = self.get_seguimientos(soup)
+
+            self.save_seguimientos(events, codigo)
+            for j in events:
+                print(j)
+            break
+
