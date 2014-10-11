eb5e99778b904e7d6c28e0bc4e96ac79311932b0
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-12 01:47:03 +0300

    fix arbor.js

--- proyectos_de_ley/seguimientos/utils.py
+++ proyectos_de_ley/seguimientos/utils.py
@@ -7,5 +7,6 @@ def get_proyecto_from_short_url(short_url):
     :return: Proyecto model object
     """
     res = Proyecto.objects.get(short_url=short_url)
+    print(short_url)
     return res
 
--- proyectos_de_ley/seguimientos/views.py
+++ proyectos_de_ley/seguimientos/views.py
@@ -1,7 +1,13 @@
 # -*- encoding: utf-8 -*-
+import re
+
 from django.shortcuts import render
+
+from . import utils
 
 
 # Create your views here.
 def index(request, short_url):
-    return render(request, "seguimientos/index.html", {"msg": short_url})
+    short_url = re.sub("/seguimiento/", "", short_url)
+    item = utils.get_proyecto_from_short_url(short_url)
+    return render(request, "seguimientos/index.html", {"item": item})
