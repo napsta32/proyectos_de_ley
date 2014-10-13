c7162a937fba3aef9eddb2f343a43405a95aa085
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-13 23:07:08 +0300

    avoid exception when iniciativas_agrupadas is None

--- proyectos_de_ley/seguimientos/utils.py
+++ proyectos_de_ley/seguimientos/utils.py
@@ -7,7 +7,9 @@ def get_proyecto_from_short_url(short_url):
     :return: Proyecto model object
     """
     item = Proyecto.objects.get(short_url=short_url)
-    if '{' in item.iniciativas_agrupadas:
+    print(item.iniciativas_agrupadas)
+    if item.iniciativas_agrupadas is not None and '{' in \
+            item.iniciativas_agrupadas:
         iniciativas = item.iniciativas_agrupadas.replace("{", "")
         iniciativas = iniciativas.replace("}", "")
         item.iniciativas_agrupadas = iniciativas.split(",")
