1159b0af6339728fc5d7e0193ce7c4d8a85f555c
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-14 00:07:59 +0300

    tests for utils module

--- proyectos_de_ley/seguimientos/tests/test_utils.py
+++ proyectos_de_ley/seguimientos/tests/test_utils.py
@@ -4,6 +4,11 @@ from pdl.models import Proyecto
 from seguimientos import utils
 
 
+class Object(object):
+    """Dummy class for testing."""
+    pass
+
+
 class SimpleTestSeguimientos(TestCase):
     def test_get_proyecto_from_short_url(self):
         short_url = "4zhube"
@@ -22,4 +27,24 @@ class SimpleTestSeguimientos(TestCase):
         self.assertEqual(expected['codigo'], result.codigo)
         self.assertEqual(expected['iniciativas_agrupadas'],
                          result.iniciativas_agrupadas)
+
+    def test_prepare_json_for_d3(self):
+        b = Proyecto(short_url="4zhube", numero_proyecto="02764/2013-CR",
+                     codigo="02764", titulo="Propone Ley Universitaria",
+                     fecha_presentacion="2013-10-10",
+                     iniciativas_agrupadas="{01790,01800}")
+        b.save()
+
+        item = Object()
+        item.numero_proyecto = "02764/2013-CR"
+        item.codigo = "02764"
+        item.titulo = "Propone Ley Universitaria"
+        item.iniciativas_agrupadas = ['02764']
+
+        expected = {'links': [{'target': 0, 'value': 1, 'source': 1}],
+                    'nodes': [{'name': '02764', 'url': '/p/'},
+                              {'name': '02764', 'url': '/p/4zhube'}]}
+        result = utils.prepare_json_for_d3(item)
+        self.assertEqual(expected, result)
+
 
