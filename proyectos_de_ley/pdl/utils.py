a2f19e2732e602eee04d293aab67000bce35832e
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-23 14:46:33 +0300

    catching strings for utils due to tests

--- proyectos_de_ley/pdl/utils.py
+++ proyectos_de_ley/pdl/utils.py
@@ -3,13 +3,17 @@ import re
 
 
 def convert_string_to_time(string):
-    this_time = re.sub("\+[0-9]+$", "", string)
-    try:
-        time_object = datetime.datetime.strptime(this_time, "%Y-%m-%d %H:%M:%S.%f")
-    except TypeError:
-        # This exception is only for our test that wants str not date obj
-        time_object = item.time_created
-    except ValueError:
-        time_object = datetime.datetime.strptime(this_time, "%Y-%m-%d %H:%M:%S")
+    if isinstance(string, str):
+        this_time = re.sub("\+[0-9]+$", "", string)
+        try:
+            time_object = datetime.datetime.strptime(this_time, "%Y-%m-%d %H:%M:%S.%f")
+        except TypeError:
+            # This exception is only for our test that wants str not date obj
+            time_object = item.time_created
+        except ValueError:
+            time_object = datetime.datetime.strptime(this_time, "%Y-%m-%d %H:%M:%S")
 
-    return time_object
+        return time_object
+    else:
+        # is should be a date object
+        return string
