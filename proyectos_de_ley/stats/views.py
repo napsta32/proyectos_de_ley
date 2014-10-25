db0d60d2a1e4ff3469e6f3678ada8eb1d53227fa
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-25 16:55:03 +0300

    gauge iniciativas

--- proyectos_de_ley/stats/views.py
+++ proyectos_de_ley/stats/views.py
@@ -7,9 +7,19 @@ def index(request):
     numero_de_proyectos = Proyecto.objects.all().count()
 
     without_pdf_url = Proyecto.objects.filter(pdf_url='').count()
-    percentage_withouth_pdf_url = round(without_pdf_url/numero_de_proyectos, 2)
+    percentage_without_pdf_url = round(without_pdf_url/numero_de_proyectos, 2)
+
+    without_iniciativas = Proyecto.objects.filter(
+        iniciativas_agrupadas='').count()
+    percentage_without_iniciativas = round(
+        without_iniciativas/numero_de_proyectos, 2)
 
     return render(request, "stats/index.html",
                   {'numero_de_proyectos': numero_de_proyectos,
                    'without_pdf_url': without_pdf_url,
-                   'percentage_without_pdf_url': percentage_withouth_pdf_url})
+                   'percentage_without_pdf_url': percentage_without_pdf_url,
+                   'without_iniciativas': without_iniciativas,
+                   'percentage_without_iniciativas':
+                       percentage_without_iniciativas,
+                   }
+    )
