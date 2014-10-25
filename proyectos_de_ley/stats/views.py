f961911917e8b616d746bd82612d654ffeaa73cf
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-25 16:40:12 +0300

    proyectos sin pdf_url

--- proyectos_de_ley/stats/views.py
+++ proyectos_de_ley/stats/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from pdl.models import Proyecto
 
 
 def index(request):
-    print("hola")
-    return render(request, "stats/index.html")
+    numero_de_proyectos = Proyecto.objects.all().count()
+
+    without_pdf_url = Proyecto.objects.filter(pdf_url='').count()
+    percentage_withouth_pdf_url = round(without_pdf_url/numero_de_proyectos, 2)
+
+    return render(request, "stats/index.html",
+                  {'numero_de_proyectos': numero_de_proyectos,
+                   'without_pdf_url': without_pdf_url,
+                   'percentage_without_pdf_url': percentage_withouth_pdf_url})
