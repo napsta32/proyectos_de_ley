92f8e49ccdaf1ba2899135b13a6394752cafc528
aniversarioperu <aniversarioperu1@gmail.com>
2014-11-08 00:22:14 +0200

    added css

--- proyectos_de_ley/search_advanced/forms.py
+++ proyectos_de_ley/search_advanced/forms.py
@@ -1,15 +1,22 @@
+from functools import partial
+
 from django import forms
 from django.core.exceptions import ValidationError
 
 from pdl.models import Proyecto
 
 
+DateInput = partial(forms.DateInput, {'class': 'datepicker'})
+
+
 class SearchAdvancedForm(forms.Form):
     date_from = forms.DateField(
+        widget=DateInput(),
         label="Fecha inicio",
         required=False,
     )
     date_to = forms.DateField(
+        widget=DateInput(),
         label="Fecha fin",
         required=False,
     )
