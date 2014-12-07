b015fce5f62c76037a836ed0b34d6155e5ed3c0b
aniversarioperu <aniversarioperu1@gmail.com>
2014-12-07 23:12:00 +0200

    fix stats test

--- proyectos_de_ley/stats/tests/test_views.py
+++ proyectos_de_ley/stats/tests/test_views.py
@@ -3,6 +3,7 @@ import datetime
 from django.test import TestCase, Client
 
 from pdl.models import Proyecto
+from stats.models import Dispensed
 
 
 class TestStatsViews(TestCase):
@@ -11,6 +12,13 @@ class TestStatsViews(TestCase):
             'numero_proyecto': '02764',
             'time_created': datetime.datetime.now(),
             'fecha_presentacion': '2010-10-10',
+        })
+        Dispensed.objects.create(**{
+            'total_approved': 1444,
+            'total_dispensed': 864,
+            'dispensed_by_plenary': 23,
+            'dispensed_by_spokesmen': 12,
+            'dispensed_others': 11,
         })
 
     def test_index(self):
