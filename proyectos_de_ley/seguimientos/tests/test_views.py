86534bb148cc3d0ec2058ce7ef271f86532e84be
aniversarioperu <aniversarioperu1@gmail.com>
2014-12-16 00:02:21 +0200

    test serializers

--- proyectos_de_ley/seguimientos/tests/test_views.py
+++ proyectos_de_ley/seguimientos/tests/test_views.py
@@ -2,7 +2,8 @@ import json
 
 from django.test import TestCase, Client
 
-from pdl.models import Proyecto, Seguimientos
+from pdl.models import Proyecto
+from pdl.models import Seguimientos
 
 
 class TestViews(TestCase):
@@ -13,22 +14,19 @@ class TestViews(TestCase):
             'fecha_presentacion': '2010-10-10',
             'short_url': '4huj5x',
             'id': 1,
-            }
-        )
+        })
         b.save()
 
         Seguimientos.objects.create(**{
             'fecha': '2011-01-01',
             'evento': "Decretado a... Trabajo y Seguridad Social",
             'proyecto': b,
-            }
-        )
+        })
         Seguimientos.objects.create(**{
             'fecha': '2012-02-02',
             'evento': "En comisi\u00f3n Trabajo y Seguridad Social",
             'proyecto': b,
-            }
-        )
+        })
 
     def test_index_view(self):
         c = Client()
@@ -49,6 +47,7 @@ class TestViews(TestCase):
     def test_api_iniciativas(self):
         c = Client()
         response = c.get('/api/iniciativas/4huj5x', follow=True)
+        print(response.content)
         self.assertEqual(200, response.status_code)
 
         response = c.get('/api/iniciativas/4huj5xaaaa', follow=True)
