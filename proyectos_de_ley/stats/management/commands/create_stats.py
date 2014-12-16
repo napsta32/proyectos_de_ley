0f250794ac775d2af0e1ff948b908d551a6f8aff
aniversarioperu <aniversarioperu1@gmail.com>
2014-12-16 23:18:12 +0200

    avoid using distinct due to SQLite

--- proyectos_de_ley/stats/management/commands/create_stats.py
+++ proyectos_de_ley/stats/management/commands/create_stats.py
@@ -33,19 +33,23 @@ class Command(BaseCommand):
                 this_comision = re.sub("\s+y\s+.+", "", this_comision)
                 comisiones.add(this_comision)
 
-        queryset = Seguimientos.objects.order_by('proyecto_id', '-fecha').distinct('proyecto_id')
+        queryset = Seguimientos.objects.order_by('proyecto_id', '-fecha')
         comisiones_count = {}
+
+        this_proyecto_id = ''
         for i in queryset:
-            for comision in comisiones:
-                if comision in i.evento:
-                    if comision not in comisiones_count:
-                        comisiones_count[comision] = 0
-                    comisiones_count[comision] += 1
+            if i.proyecto_id != this_proyecto_id:
+                for comision in comisiones:
+                    if comision in i.evento:
+                        if comision not in comisiones_count:
+                            comisiones_count[comision] = 0
+                        comisiones_count[comision] += 1
+            this_proyecto_id = i.proyecto_id
+
         for k, v in comisiones_count.items():
-            obj, created = ComisionCount.objects.update_or_create(
-                comision=k, count=v
+            ComisionCount.objects.update_or_create(
+                comision=k, defaults={'count': v},
             )
-            print(obj, created)
 
         # Get projects that did not go to 2da round of votes
         self.get_dispensed_projects()
@@ -54,9 +58,9 @@ class Command(BaseCommand):
         total_approved = Seguimientos.objects.filter(evento__icontains='aprobado').count()
         total_dispensed = Seguimientos.objects.filter(evento__icontains='dispensado 2da').count()
         dispensed_by_plenary = Seguimientos.objects.filter(
-                evento__icontains='dispensado 2da').filter(evento__icontains='pleno').count()
+            evento__icontains='dispensado 2da').filter(evento__icontains='pleno').count()
         dispensed_by_spokesmen = Seguimientos.objects.filter(
-                evento__icontains='dispensado 2da').filter(evento__icontains='portavoces').count()
+            evento__icontains='dispensado 2da').filter(evento__icontains='portavoces').count()
         dispensed_others = total_dispensed - dispensed_by_plenary - dispensed_by_spokesmen
 
         Dispensed.objects.update_or_create(
