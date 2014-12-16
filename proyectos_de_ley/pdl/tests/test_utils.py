ce845c01ff12308cf2e94f6a06377ee078085cdc
aniversarioperu <aniversarioperu1@gmail.com>
2014-12-16 17:31:05 +0200

    test timer

--- proyectos_de_ley/pdl/tests/test_utils.py
+++ proyectos_de_ley/pdl/tests/test_utils.py
@@ -1,8 +1,10 @@
+import time
 import datetime
 
 from django.test import TestCase
 
 from pdl.utils import convert_string_to_time, convert_date_to_string
+from pdl.utils import Timer
 
 
 class TestUtils(TestCase):
@@ -31,3 +33,21 @@ class TestUtils(TestCase):
         date = "2014-10-28"
         result = convert_date_to_string(date)
         self.assertIsNone(result)
+
+
+class TestTimer(TestCase):
+    def test_timer(self):
+        with Timer() as t:
+            time.sleep(1)
+
+        expected = 1
+        result = round(t.secs)
+        self.assertEqual(expected, result)
+
+    def test_timer_verbose(self):
+        with Timer(verbose=True) as t:
+            time.sleep(1)
+
+        expected = 1
+        result = round(t.secs)
+        self.assertEqual(expected, result)
