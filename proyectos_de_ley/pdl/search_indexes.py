3de9bd7278cde6386451a62b9670b2e7a5bfc538
aniversarioperu <aniversarioperu1@gmail.com>
2015-03-03 01:17:40 +0200

    index ascii and accented chars for title

--- proyectos_de_ley/pdl/search_indexes.py
+++ proyectos_de_ley/pdl/search_indexes.py
@@ -1,3 +1,5 @@
+import unicodedata
+
 from haystack import indexes
 
 from .models import Proyecto
@@ -6,6 +8,13 @@ from .models import Proyecto
 class ProyectoIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     date = indexes.DateField(model_attr='fecha_presentacion', null=True)
+
+    def prepare_text(self, obj):
+        data = [obj.codigo, obj.titulo]
+        original = ' '.join(data)
+        modified = unicodedata.normalize('NFD', original).encode('ascii', 'ignore')
+        result = ' '.join([original, modified.decode(encoding='utf-8')])
+        return result
 
     def get_model(self):
         return Proyecto
