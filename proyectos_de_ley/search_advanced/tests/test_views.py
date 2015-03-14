3c20fc2cb92148719e94db3ef9813c1934419e38
aniversarioperu <aniversarioperu1@gmail.com>
2015-03-15 01:17:10 +0200

    increase coverage, search by comission

--- proyectos_de_ley/search_advanced/tests/test_views.py
+++ proyectos_de_ley/search_advanced/tests/test_views.py
@@ -1,9 +1,26 @@
+import json
+import os
+
 from django.test import TestCase, Client
+
+from pdl.models import Proyecto
+from pdl.models import Seguimientos
 
 
 class TestSearchAdvancedViews(TestCase):
     def setUp(self):
-        pass
+        self.maxDiff = None
+
+        this_folder = os.path.abspath(os.path.dirname(__file__))
+        dummy_db_json = os.path.join(this_folder, '..', '..', 'pdl', 'tests', 'dummy_db.json')
+        dummy_items = json.loads(open(dummy_db_json, 'r').read())
+
+        for i in dummy_items:
+            b = Proyecto(**i)
+            b.save()
+            s = Seguimientos(proyecto=b, fecha='2014-06-23',
+                             evento='Decreado a... Ciencia, Innovación y Tecnología')
+            s.save()
 
     def test_index(self):
         c = Client()
@@ -23,4 +40,4 @@ class TestSearchAdvancedViews(TestCase):
     def test_index_search_comission(self):
         c = Client()
         response = c.get('/search-advanced/?comision=Ciencia')
-        self.assertEqual(200, response.status_code)
+        self.assertTrue('arco y flecha' in str(response.content))
