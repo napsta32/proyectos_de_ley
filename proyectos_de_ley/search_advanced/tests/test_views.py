22afb41be593b2f3634eb44586878b7cef470c4b
aniversarioperu <aniversarioperu1@gmail.com>
2015-03-15 01:02:02 +0200

    test search by comission

--- proyectos_de_ley/search_advanced/tests/test_views.py
+++ proyectos_de_ley/search_advanced/tests/test_views.py
@@ -2,6 +2,9 @@ from django.test import TestCase, Client
 
 
 class TestSearchAdvancedViews(TestCase):
+    def setUp(self):
+        pass
+
     def test_index(self):
         c = Client()
         response = c.get('/search-advanced/')
@@ -10,4 +13,14 @@ class TestSearchAdvancedViews(TestCase):
     def test_index_form_invalid(self):
         c = Client()
         response = c.get('/search-advanced/?date_from=hola&date_to=12%2F19%2F2014')
+        self.assertEqual(200, response.status_code)
+
+    def test_index_search_date(self):
+        c = Client()
+        response = c.get('/search-advanced/?date_from=10/03/2014&date_to=01/01/2015')
+        self.assertEqual(200, response.status_code)
+
+    def test_index_search_comission(self):
+        c = Client()
+        response = c.get('/search-advanced/?comision=Ciencia')
         self.assertEqual(200, response.status_code)
