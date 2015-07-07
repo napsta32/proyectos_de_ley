554eb17b12f51e3774912d06cd63df584d22f59d
aniversarioperu <aniversarioperu1@gmail.com>
2015-07-07 16:40:41 +0300

    fixing seguimientos api for internal use

--- proyectos_de_ley/seguimientos/views.py
+++ proyectos_de_ley/seguimientos/views.py
@@ -20,7 +20,14 @@ def index(request, short_url):
     short_url = re.sub("/seguimiento/", "", short_url)
     item = utils.get_proyecto_from_short_url(short_url)
     item.expediente_events = utils.get_events_from_expediente(item.id)
-    return render(request, "seguimientos/index.html", {"item": item})
+
+    # TODO: arreglar esto para cuanto tengamos proyectos de la legislatura 2016
+    friendly_code = str(item.codigo) + '-2011'
+    return render(request, "seguimientos/index.html",
+                  {
+                      "item": item,
+                      "friendly_code": friendly_code,
+                  })
 
 
 class JSONResponse(HttpResponse):
