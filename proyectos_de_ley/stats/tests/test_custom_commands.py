68803b68b1989ec6bf84f3016470ddc04d5372dd
aniversarioperu <aniversarioperu1@gmail.com>
2015-07-11 16:12:18 +0300

    tests for approved

--- proyectos_de_ley/stats/tests/test_custom_commands.py
+++ proyectos_de_ley/stats/tests/test_custom_commands.py
@@ -4,6 +4,7 @@ from django.core.management import call_command
 from pdl.models import Proyecto
 from pdl.models import Seguimientos
 from stats.models import ComisionCount
+from stats.models import Dispensed
 
 
 class TestCustomCommand(TestCase):
@@ -29,9 +30,27 @@ class TestCustomCommand(TestCase):
             'evento': 'en comisión Justicia',
             'proyecto': b,
         })
+        c1 = Seguimientos(**{
+            'fecha': '2012-10-10',
+            'evento': 'Dispensado 2da Votación',
+            'proyecto': b,
+        })
+        c2 = Seguimientos(**{
+            'fecha': '2012-10-10',
+            'evento': 'Promulgado Ley No: 29971',
+            'proyecto': b,
+        })
+        c3 = Seguimientos(**{
+            'fecha': '2012-10-10',
+            'evento': 'Publicado: Ley No: 29971',
+            'proyecto': b,
+        })
         b.save()
         b1.save()
         c.save()
+        c1.save()
+        c2.save()
+        c3.save()
         call_command('create_stats')
 
     def test_create_stats(self):
@@ -45,3 +64,13 @@ class TestCustomCommand(TestCase):
         expected = 'Titulo de Ley'
         result = res.titulo_de_ley
         self.assertEqual(expected, result)
+
+    def test_get_dispensados_2da_votacion(self):
+        res = Dispensed.objects.all()
+        expected = 1
+        self.assertEqual(expected, res[0].total_dispensed)
+
+    def test_get_aprobados(self):
+        res = Dispensed.objects.all()
+        expected = 1
+        self.assertEqual(expected, res[0].total_approved)
