9c3e64d4d07c44444d74190ed992097c604a69ad
aniversarioperu <aniversarioperu1@gmail.com>
2015-07-13 01:25:37 +0300

    fix searching normalized data in index

--- proyectos_de_ley/pdl/forms.py
+++ proyectos_de_ley/pdl/forms.py
@@ -1,11 +1,12 @@
 import unicodedata
 
-from haystack.forms import HighlightedSearchForm
+from haystack.forms import SearchForm
 
 
-class SimpleSearchForm(HighlightedSearchForm):
+class SimpleSearchForm(SearchForm):
     def search(self):
         sqs = super(SimpleSearchForm, self).search()
+        print(">>>sqws", sqs)
 
         if not self.is_valid():
             return self.no_query_found()
@@ -13,8 +14,13 @@ class SimpleSearchForm(HighlightedSearchForm):
         if not self.cleaned_data.get('q'):
             return self.no_query_found()
 
-        query = unicodedata.normalize('NFD', self.cleaned_data['q']).encode('ascii', 'ignore').decode(encoding='utf-8')
+        """
+        original = self.cleaned_data['q']
+        modified = unicodedata.normalize('NFKD', original).encode('ascii', 'ignore')
+        query = modified.decode(encoding='utf-8')
+        print(">>>>>query form", query)
         sqs = self.searchqueryset.auto_query(query).order_by('-codigo')
+        """
 
         if self.load_all:
             sqs = sqs.load_all()
