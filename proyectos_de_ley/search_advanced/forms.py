986e7f371d636760f92aa47663193903a75c3a50
aniversarioperu <aniversarioperu1@gmail.com>
2015-07-13 12:12:00 +0300

    por bancada en forms

--- proyectos_de_ley/search_advanced/forms.py
+++ proyectos_de_ley/search_advanced/forms.py
@@ -2,6 +2,7 @@ from functools import partial
 
 from django import forms
 
+from pdl.models import Proyecto
 from pdl.models import Slug
 
 
@@ -56,6 +57,23 @@ class SearchAdvancedForm(forms.Form):
             ('Vivienda', 'Vivienda'),
         ]
     )
+    congresista = forms.ModelChoiceField(
+        Slug.objects.all().order_by('nombre'),
+        label='Búsqueda por author de proyecto de ley.',
+        required=False,
+        empty_label='--Escoger nombre--',
+        widget=forms.Select(attrs={'class': 'form-control'}),
+    )
+    grupo_parlamentario = forms.ModelChoiceField(
+        Proyecto.objects.filter(
+            grupo_parlamentario__isnull=False).exclude(
+            grupo_parlamentario='').values_list(
+            'grupo_parlamentario', flat=True).order_by('grupo_parlamentario').distinct(),
+        label='Búsqueda por grupo parlamentario.',
+        required=False,
+        empty_label='--Escoger bancada--',
+        widget=forms.Select(attrs={'class': 'form-control'}),
+    )
     dispensados_2da_votacion = forms.ChoiceField(
         widget=forms.Select(attrs={'class': 'form-control'}),
         label='Dispensados 2da votación',
@@ -79,11 +97,4 @@ class SearchAdvancedForm(forms.Form):
             ('NÚMERO TOTAL DE LEYES', 'NÙMERO TOTAL DE LEYES'),
             ('Exonerados de dictamen', 'Exonerados de dictamen'),
         ]
-    )
-    congresista = forms.ModelChoiceField(
-        Slug.objects.all().order_by('nombre'),
-        label='Búsqueda por author de proyecto de ley.',
-        required=False,
-        empty_label='--Escoger nombre--',
-        widget=forms.Select(attrs={'class': 'form-control'}),
     )
