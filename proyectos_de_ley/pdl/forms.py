464c66e1699506c885b7ea62339baf7f9b0df013
aniversarioperu <aniversarioperu1@gmail.com>
2015-07-14 00:34:40 +0300

    clean form

--- proyectos_de_ley/pdl/forms.py
+++ proyectos_de_ley/pdl/forms.py
@@ -1,26 +1,14 @@
-import unicodedata
-
 from haystack.forms import SearchForm
 
 
 class SimpleSearchForm(SearchForm):
     def search(self):
         sqs = super(SimpleSearchForm, self).search()
-        print(">>>sqws", sqs)
-
         if not self.is_valid():
             return self.no_query_found()
 
         if not self.cleaned_data.get('q'):
             return self.no_query_found()
-
-        """
-        original = self.cleaned_data['q']
-        modified = unicodedata.normalize('NFKD', original).encode('ascii', 'ignore')
-        query = modified.decode(encoding='utf-8')
-        print(">>>>>query form", query)
-        sqs = self.searchqueryset.auto_query(query).order_by('-codigo')
-        """
 
         if self.load_all:
             sqs = sqs.load_all()
