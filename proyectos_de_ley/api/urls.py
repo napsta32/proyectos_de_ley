c9e9ef5f4d29417fe0566a16b66863bba2d689fd
aniversarioperu <aniversarioperu1@gmail.com>
2015-07-14 21:44:17 +0300

    use json

--- proyectos_de_ley/api/urls.py
+++ proyectos_de_ley/api/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^proyecto/(?P<codigo>[0-9]+\-[0-9]+)/$', views.proyecto),
-    url(r'^congresista/(?P<nombre_corto>.+)/$', views.congresista),
+    url(r'^congresista.json/(?P<nombre_corto>.+)/$', views.congresista),
     url(r'^congresista_y_comision/(?P<nombre_corto>.+)/(?P<comision>.+)/$', views.congresista_y_comision),
     url(r'^exonerados_dictamen/$', views.exonerados_dictamen),
     url(r'^exonerados_2da_votacion/$', views.exonerados_2da_votacion),
