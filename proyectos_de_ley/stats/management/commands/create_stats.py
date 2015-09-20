a75645b140c1a8acaca661481b3c42ec3c56616b
aniversarioperu <aniversarioperu1@gmail.com>
2015-09-20 12:35:44 +0300

    use project

--- proyectos_de_ley/stats/management/commands/create_stats.py
+++ proyectos_de_ley/stats/management/commands/create_stats.py
@@ -40,15 +40,15 @@ class Command(BaseCommand):
 
         queryset = Seguimientos.objects.order_by('proyecto_id', '-fecha')
         commissions_count = {}
-        this_proyecto_id = ''
+        this_project_id = ''
         for seguimiento in queryset:
-            if seguimiento.proyecto_id != this_proyecto_id:
+            if seguimiento.proyecto_id != this_project_id:
                 for commission in commissions:
                     if commission in seguimiento.evento:
                         if commission not in commissions_count:
                             commissions_count[commission] = 0
                         commissions_count[commission] += 1
-            this_proyecto_id = seguimiento.proyecto_id
+            this_project_id = seguimiento.proyecto_id
 
         ComisionCount.objects.all().delete()
         for k, v in commissions_count.items():
