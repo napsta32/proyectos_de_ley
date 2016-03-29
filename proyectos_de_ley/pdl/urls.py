b801dd4dc2b6c02e01d486b14d37546bcc502ab9
carlosp420 <mycalesis@gmail.com>
2016-03-29 00:25:55 -0500

    upgrade url patterns

--- proyectos_de_ley/pdl/urls.py
+++ proyectos_de_ley/pdl/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-from . import views
+from .views import index, about, search, congresista, proyecto
 
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^congresista/(?P<congresista_slug>.*)$', views.congresista,
+urlpatterns = [
+    # '',
+    url(r'^$', index, name='index'),
+    url(r'^about/$', about, name='about'),
+    url(r'^search/$', search, name='search'),
+    url(r'^congresista/(?P<congresista_slug>.*)$', congresista,
         name='congresista'),
 
-    url(r'^(?P<short_url>[0-9a-z]*)/$', views.proyecto),
-)
+    url(r'^(?P<short_url>[0-9a-z]*)/$', proyecto),
+]
--- proyectos_de_ley/search_advanced/urls.py
+++ proyectos_de_ley/search_advanced/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-from . import views
+from .views import index
 
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', views.index),
-)
+urlpatterns = [
+    # '',
+    url(r'^$', index),
+]
