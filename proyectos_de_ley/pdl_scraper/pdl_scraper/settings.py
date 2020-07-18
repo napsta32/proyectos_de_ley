e82432ed70372285a0b219b71272a588f2fd5e21
Carlos Peña <mycalesis@gmail.com>
2020-07-18 17:59:54 -0500

    integrate crawler

--- proyectos_de_ley/pdl_scraper/pdl_scraper/settings.py
+++ proyectos_de_ley/pdl_scraper/pdl_scraper/settings.py
@@ -7,42 +7,21 @@
 #
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
-import json
-import os
-from unipath import Path
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath('.')))
+# Do not forget the change iCrawler part based on your project name
+os.environ['DJANGO_SETTINGS_MODULE'] = 'proyectos_de_ley.settings.local'
+
+# This is required only if Django Version > 1.8
+import django
+django.setup()
+
+import json
+from unipath import Path
 
 
-BASE_DIR = Path(__file__).absolute().ancestor(3)
-SECRETS_FILE = os.path.join(BASE_DIR, 'config.json')
-
-with open(SECRETS_FILE) as f:
-    secrets = json.loads(f.read())
-
-
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {0} environment variable".format(setting)
-        print(error_msg)
-        sys.exit(1)
-
-
-# remember to setup a password for postgres user account, see:
-# http://stackoverflow.com/a/7696398/3605870
-
-# also create a test_pdl for unittests
-DATABASE = {
-    'drivername': get_secret('drivername'),
-    'username': get_secret('username'),
-    'password': get_secret('password'),
-    'host': get_secret('host'),
-    'port': get_secret('port'),
-    'database': get_secret('database'),
-}
-
-LEGISLATURE = get_secret("legislature")
+LEGISLATURE = '2016'
 
 BOT_NAME = 'pdl_scraper'
 
@@ -66,18 +45,10 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 10
 DOWNLOAD_DELAY = 2
 
 
-CRAWLERA_USER = get_secret("crawlera_user")
-CRAWLERA_PASS = get_secret("crawlera_pass")
 DOWNLOADER_MIDDLEWARES = {
     'scrapylib.crawlera.CrawleraMiddleware': 600,
     'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36",
-    'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': "http://" + CRAWLERA_USER + ":" + CRAWLERA_PASS + "@proxy.crawlera.com:8010/",
 }
-
-if get_secret("crawlera_enabled") == 'true':
-    CRAWLERA_ENABLED = True
-else:
-    CRAWLERA_ENABLED = False
 
 
 LOG_LEVEL = 'DEBUG'
