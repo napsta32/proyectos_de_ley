e57d4c7f29cda832a7e8bbd01793cfc1b230e6b8
Carlos Peña <mycalesis@gmail.com>
2020-07-26 13:44:17 -0500

    try to fix tests

--- proyectos_de_ley/proyectos_de_ley/settings/testing.py
+++ proyectos_de_ley/proyectos_de_ley/settings/testing.py
@@ -1,5 +1,17 @@
 from .base import *
 import sys
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'travis_ci_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 print(TESTING)
